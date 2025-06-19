09bfdd8981307a75ec3b3771f25a63882a4e0de3
anniewanjohi <annieww24@gmail.com>
2025-06-19 22:16:54 +0300

    view class

---
+++ View.java
@@ -0,0 +1,11 @@
+// View.java
+public class View implements OperationStrategy {
+
+    @Override
+    public void execute() {
+        System.out.println("Some data to be displayed.");
+    }
+}
+
+    
+
