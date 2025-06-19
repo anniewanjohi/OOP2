7687aa65f3bf22b06b6311c716f89be9219cc962
anniewanjohi <annieww24@gmail.com>
2025-06-19 22:14:25 +0300

    delete data

---
+++ Delete.java
@@ -0,0 +1,11 @@
+// Delete.java
+public class Delete implements OperationStrategy {
+
+    @Override
+    public void execute() {
+        System.out.println("Data has been deleted.");
+    }
+}
+
+    
+
