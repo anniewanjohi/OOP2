c76a417bfe79c16442f0f62bbf1286181a7484f0
anniewanjohi <annieww24@gmail.com>
2025-06-19 22:15:27 +0300

    main application

---
+++ Mainapplication.java
@@ -0,0 +1,15 @@
+// MainApplication.java
+public class Mainapplication {
+
+    public static void main(String[] args) {
+
+        Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");
+        User curUser = new User(4455, "John");
+
+        curAdmin.performOperation(new Update()); // Admin updates the data.
+        curUser.performOperation(new View());    // User views the data.
+
+        // Optionally: Admin deletes data
+        curAdmin.performOperation(new Delete());
+    }
+}
