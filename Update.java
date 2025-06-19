fe5237901d6f84b29e355b9b58556a8235dcb42a
anniewanjohi <annieww24@gmail.com>
2025-06-19 22:16:23 +0300

    update data

---
+++ Update.java
@@ -0,0 +1,11 @@
+// Update.java
+public class Update implements OperationStrategy {
+
+    @Override
+    public void execute() {
+        System.out.println("Data has been updated successfully!");
+    }
+}
+
+    
+
