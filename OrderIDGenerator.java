3476ec009f0db58c656a6c9661ee73b861b57ddd
anniewanjohi <annieww24@gmail.com>
2025-06-19 22:16:06 +0300

    OrderId Generation

---
+++ OrderIDGenerator.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class OrderIDGenerator {
+    public static void main(String[] args){
+        Random random =new Random();
+        String[] orderIDs=new String[10];
+        for (int i=0;i<orderIDs.length;i++){
+            char prefix =(char)(random.nextInt(5)+ 'A');
+            int number=random.nextInt(99) +1;
+            String suffix =String.format("%03d",number);
+            orderIDs[i] =prefix + suffix;
+        }
+        for (String orderID :orderIDs){
+            System.out.println(orderID);
+        }
+    }
+}
