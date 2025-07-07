3f30d57bc86cef8df7ed8c3e22ab082305015cdf
anniewanjohi <annieww24@gmail.com>
2025-07-07 16:32:59 +0300

    sum using recursion

---
+++ Sum.java
@@ -0,0 +1,15 @@
+
+public class Sum {
+    public static int sum(int n) {
+        if (n== 1){
+            return 1;
+        }else {
+            return n + sum(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = sum(5);
+        System.out.println("The sum is: " + result);
+    }
+}
